a90daf62f876b388c018ac6b39b5872718a02d0d
Gautier-c <gautier.couturier@gmail.com>
2020-06-05 12:40:49 +0200

    changemement formulaire, manque regex email

--- javascript_cart.js
+++ javascript_cart.js
@@ -45,25 +45,38 @@ pdtContainer.appendChild(totalBasket);
 //FORMULAIRE
 
 //validation formulaire via bouton valider
-let erreur = ['nom','prenom','tel','mail','address','codep','ville'];
-let erreurMaj = ['Nom','Prenom','Tel','Mail','Address','Codep','Ville'];
-let formValid = document.getElementById('boutonEnvoi');
-for ( i of erreur)
-{
-    let nom = document.getElementById([i]);
-    let erreurMaj = ['Nom','Prenom','Tel','Mail','Address','Codep','Ville'];
-
-    for (i of erreurMaj)
-    {
-        let missNom = document.getElementById('miss'+ [i])
-        formValid.addEventListener('click', validation);
-
-        function validation(event){
-        //si le champ est vide
-        if (nom.validity.valueMissing){
-            event.preventDefault();
-            missNom.textContent ="Veuillez remplir cet information avant de commander";
-            missNom.style.color = 'red'
-        }
+function validateEmail(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
     }
-}}
+    
+    function Verification() {
+    // Récupérer lavaleur des champs nom et email
+    var Nom = document.getElementById('idNom').value;
+    var Email = document.getElementById('idEmail').value;
+    
+    // Contrôle sur le nom
+    if(Nom==''){
+    alert('Vous devez compléter votre nom !');
+    document.getElementById('idNom').style.backgroundColor="red";
+    document.getElementById('idNom').style.color="#FFF";
+    
+    // Permet de bloquer l'envoi du formulaire
+    return false;
+    }
+    else{
+    document.getElementById('idNom').style.backgroundColor="#9C6";
+    }
+    
+    // Contrôle sur l'email
+    if(Email=='') {
+    alert('Vous devez compléter votre adresse email');
+    document.getElementById('idEmail').style.backgroundColor="red";
+    document.getElementById('idEmail').style.color="#FFF";
+    return false;
+    }
+    
+    else{
+    document.getElementById('idEmail').style.backgroundColor="#9C6";
+    }
+}
